3f42551dbcb2ecc803f3a6723493a5cd20ce8d82
Yorty Ruiz Hernandez <yorty.hernandez@surlatable.com>
2024-01-14 19:48:59 -0500

    no env validation needed.

--- apps/common/src/common.module.ts
+++ apps/common/src/common.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './health/health.module';
 import { ConfigModule } from '@nestjs/config';
-import { validate } from './env.validation';
 import { LoggerModule } from 'nestjs-pino';
 
 @Module({
@@ -11,7 +10,6 @@ import { LoggerModule } from 'nestjs-pino';
     ConfigModule.forRoot({
       cache: true,
       isGlobal: true,
-      validate,
     }),
   ],
   controllers: [],
