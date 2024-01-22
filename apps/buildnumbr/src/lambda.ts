5ff04630edc93d4590c601c41f834689ff8df019
Yorty Ruiz <yorty.ruiz@gmail.com>
2024-01-21 21:26:04 -0500

    Adding warmer

--- apps/buildnumbr/src/lambda.ts
+++ apps/buildnumbr/src/lambda.ts
@@ -1,11 +1,15 @@
 import { Callback, Context, Handler } from 'aws-lambda';
 import serverless from 'serverless-http';
 import { bootstrapApp } from './bootstrap';
+import { Logger } from 'nestjs-pino';
+
+let logger: Logger;
 
 async function bootstrap(): Promise<Handler> {
   const app = await bootstrapApp();
   await app.init();
   const expressApp = app.getHttpAdapter().getInstance();
+  logger = app.get(Logger);
   return serverless(expressApp);
 }
 
@@ -18,6 +22,10 @@ export const handler: Handler = async (
 ) => {
   if (!bootstrappedHandler) {
     bootstrappedHandler = await bootstrap();
+  }
+  if (event.source === 'serverless-plugin-warmup') {
+    logger?.log('WarmUp - Lambda is warm!');
+    return 'Lambda is warm!';
   }
   return bootstrappedHandler(event, context, callback);
 };
